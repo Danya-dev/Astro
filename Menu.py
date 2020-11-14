f993501451d93b27b4d7d905bc45e78f122f2c1a
PhilippKT <kirakosiants.ft@phystech.edu>
2020-11-14 13:50:38 +0300

    Создан модуль меню и описаны его функции.

---
+++ Menu.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 14 13:44:05 2020
+
+@author: Филипп
+"""
+
+Class Menu():
+    '''Класс меню. Реализует отрисовку меню и функции меню.'''
+    def __init__(self):
+        pass
+    
+    def draw(self):
+        pass
+    
+    def botton(self):
+        pass
+    
+    def levels(self):
+        pass
+    
+    def setting(self):
+        pass
+    
+    def info(self):
+        pass
