b5133cc847b64277c79a212cbb31af636c867f7c
PhilippKT <kirakosiants.ft@phystech.edu>
2020-11-14 15:22:29 +0300

    Реализована отрисовка меню.

--- Menu.py
+++ Menu.py
@@ -4,17 +4,24 @@ Created on Sat Nov 14 13:44:05 2020
 
 @author: Филипп
 """
+import pygame as pg
+import Botton
 
-Class Menu():
+pg.init()
+
+class Menu():
     '''Класс меню. Реализует отрисовку меню и функции меню.'''
-    def __init__(self):
-        pass
-    
+    def __init__(self, screen):
+        self.screen = screen
+        self.levels = Botton(self.screen, 100, 50, 60, 40, (255, 0, 0), "Levels")
+        self.settings = Botton(self.screen, 100, 100, 60, 40, (255, 0, 0), "Settings")
+        self.info = Botton(self.screen, 100, 150, 60, 40, (255, 0, 0), "Information")
+        
     def draw(self):
-        pass
-    
-    def botton(self):
-        pass
+        screen.fill((0, 0, 0))
+        self.levels.draw()
+        self.setting.draw()
+        self.info.draw()
     
     def levels(self):
         pass
