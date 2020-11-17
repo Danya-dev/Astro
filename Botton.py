af24a152503f2712f9503247984f04dc54569c6b
PhilippKT <kirakosiants.ft@phystech.edu>
2020-11-17 20:41:15 +0300

    Исправлен модуль Botton.py

--- Botton.py
+++ Botton.py
@@ -8,7 +8,7 @@ import pygame as pg
 
 pg.init()
 
-Class Botton():
+class Botton():
     '''Класс кнопки.'''
     def __init__(self, screen, coords, width, height, color, text):
         self.screen = screen
@@ -25,14 +25,17 @@ Class Botton():
         pg.draw.rect(self.screen, self.color, (ltop_corner[0], ltop_corner[1],
                     self.width, self.height))
         botton_text = self.font.render(self.text, True, (255, 255, 255))
-        screen.blit(botton_text, (ltop_corner[0], self.coords[1]))
+        self.screen.blit(botton_text, (ltop_corner[0], self.coords[1]))
         
     
     def click(self, mouse_coord):
         if (mouse_coord[0] >= self.coords[0] - self.width / 2) & (
             mouse_coord[0] <= self.coords[0] + self.width / 2) & (
             mouse_coord[1] >= self.coords[1] - self.height / 2) & (
-            mouse_coord[1] <= self.coords[1] + self.width / 2):
+            mouse_coord[1] <= self.coords[1] + self.height / 2):
                     return True
         else:
             return False
+        
+def init(screen, coords, width, height, color, text):
+    return Botton(screen, coords, width, height, color, text)
